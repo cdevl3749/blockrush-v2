a820e993c18f4f3fc8f78d75c892ef745bc1b727
cdevl3749 <cdevl3749@gmail.com>
2025-11-22 20:40:31 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/blockrush-v2/", // <= IMPORTANT pour GitHub Pages
+  base: "/blockrush-v2/", // CORRECT pour GitHub Pages
 });
 
