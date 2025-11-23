2dc706398697b75cf3cfb82f7db0b1f56155813f
cdevl3749 <cdevl3749@gmail.com>
2025-11-23 01:10:39 +0100

    Fix dist paths

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/blockrush-v2/",
   build: {
     outDir: "dist",
   },
 });
+
 
 
 
